a002eefa5852822938509d4970e65f0d320111fc
merryt <tymerry@gmail.com>
2025-04-17 21:18:35 +0200

    switching the name

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Tyler's BJJ Knowledge",
+    pageTitle: "TCM BJJ",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
